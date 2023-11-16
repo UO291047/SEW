7b11dd72b05043c064057704f077999bd8bb443b
UO291047 <145019884+UO291047@users.noreply.github.com>
2023-11-16 14:56:07 +0100

    Add files via upload

---
+++ js/fondo.js
@@ -0,0 +1,7 @@
+class Fondo{
+    constructor(nombre_pais, nombre_capital, coords){
+        this.nombre_pais = nombre_pais;
+        this.nombre_capital = nombre_capital;
+        this.coords = coords;
+    }
+}
