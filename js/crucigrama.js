36eeebe1edb0f47e2c5c093e0613b6c65132fdf9
UO291047 <uo291047@uniovi.es>
2023-11-29 18:08:52 +0100

    Crucigrama terminado
    Aunque no funciona aún jejeje

--- js/crucigrama.js
+++ js/crucigrama.js
@@ -92,59 +92,160 @@ class Crucigrama{
     
         // Obtiene el código de la tecla pulsada
         const keyCode = event.keyCode;
+        var element = 0;
     
         // Verifica si la tecla pulsada es un número (códigos de teclas numéricas de 1 a 9)
         if (keyCode >= 49 && keyCode <= 57) {
             // Obtiene el número de la tecla pulsada
-            const number = keyCode - 48;
+            element = keyCode - 48;
     
             // Llama al método introduceNumber con el número obtenido
-            this.introduceNumber(number);
-        }else if (keyCode === 8) {
-            // Si la tecla pulsada es Backspace, llama al método introduceNumber con el valor 0
-            this.introduceNumber(0);
+            this.introduceElement(element);
+            return;
+        }// Si la tecla pulsada es Backspace, llama al método introduceNumber con el valor 0
+        else if (keyCode === 8) {
+            element = 0;
+            this.introduceElement(element);
+            return;
         }
+
+        //Operadores aritmeticos
+        if(keyCode === 111 || keyCode === 191){ // /
+            element = "/"
+            this.introduceElement(element);
+        }else if(keyCode === 106 || (keyCode === 56 && event.shiftKey)){ // *
+            element = "*";
+            this.introduceElement(element);
+        }else if(keyCode === 109 || keyCode === 189){ // -
+            element = "-";
+            this.introduceElement(element);
+        }else if(keyCode === 107 || keyCode === 187){ // +
+            element = "+";
+            this.introduceElement(element);
+        }
+
     }
 
-    introduceNumber(number) {
+    introduceElement(element) {
 
         const { row, column } = this.selectedCell;
-
         const selectedCellElement = document.querySelector(`[data-row='${row}'][data-column='${column}']`);
 
-        if(number === 0){
-            this.tablero[row][column] = number;
-            selectedCellElement.dataset.state = 'clicked';
+        var expression_row = true;
+        var expression_col = true;
+
+        this.tablero[row][column] = element;
+
+        if(element === 0){
+            selectedCellElement.dataset.state = 'init';
             selectedCellElement.textContent = "";
             return;
         }
 
-        // Comprobar si el número es válido en la fila
-        const isNumberValidInRow = !this.tablero[row].includes(number);
-
-        // Comprobar si el número es válido en la columna
-        const isNumberValidInColumn = !this.tablero.map(row => row[column]).includes(number);
-
-        // Comprobar si el número es válido en la sub-cuadrícula 3x3
-        const subgridStartRow = Math.floor(row / 3) * 3;
-        const subgridStartColumn = Math.floor(column / 3) * 3;
-        const isNumberValidInSubgrid = this.isNumberInSubgrid(subgridStartRow, subgridStartColumn, number);
+        expression_row = this.validRow(selectedCellElement);
+        expression_col = this.validCol(selectedCellElement);
 
         // Si el número es válido, realizar acciones necesarias
-        if (isNumberValidInRow && isNumberValidInColumn && isNumberValidInSubgrid) {
+        if (expression_row && expression_col) {
 
-            this.tablero[row][column] = number;
             selectedCellElement.dataset.state = 'correct';
-            selectedCellElement.textContent = number;
+            selectedCellElement.textContent = element;
 
             // Comprobar si ya están rellenas todas las cuadrículas del sudoku
-            if (this.isSudokuCompleted()) {
-                alert('¡Sudoku completado!');
+            if (this.check_win_condition()) {
+                this.end_time = Date.now();
+                const tiempo = this.calculate_date_difference();
+                alert('¡Crucigrama completado! \n Has tardado: ' + tiempo);
                 return;
             }
         } else {
+            this.tablero[row][column] = 0;
+            selectedCellElement.dataset.state = 'init';
             alert('Número no válido para la casilla seleccionada.');
         }
+    }
+
+    validRow(cell){
+        var {row, col} = cell;
+
+        if(row + 1 >= this.rows || this.tablero[row+1][col] == -1){
+            return true;
+        }
+
+        for(var i=1; i<this.rows; i++){
+            if(this.tablero[row+i][col] === "="){
+                row = row + i;
+                break;
+            }
+        }
+
+        var first_number = this.tablero[row - 3][col];
+        var second_number = this.tablero[row - 1][col];
+        var expression = this.tablero[row - 2][col];
+        var result = this.tablero[row + 1][col];
+
+        if(first_number != 0 && second_number != 0 && expression != 0 && result != 0){
+            var expr = [first_number, expression, second_number].join();
+            var res = eval(expr);
+            if(res == result){
+                return true;
+            }
+        }
+
+        return false;
+ 
+    }
+
+    validCol(cell){
+        var {row, col} = cell;
+
+        if(col + 1 >= this.columns || this.tablero[row][col+1] == -1){
+            return true;
+        }
+
+        for(var i=1; i<this.columns; i++){
+            if(this.tablero[row][col+i] === "="){
+                col = col + i;
+                break;
+            }
+        }
+
+        var first_number = this.tablero[row][col - 3];
+        var second_number = this.tablero[row][col - 1];
+        var expression = this.tablero[row][col - 2];
+        var result = this.tablero[row][col + 1];
+
+        if(first_number != 0 && second_number != 0 && expression != 0 && result != 0){
+            var expr = [first_number, expression, second_number].join();
+            var res = eval(expr);
+            if(res == result){
+                return true;
+            }
+        }
+
+        return false;
+ 
+    }
+
+    check_win_condition() {
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.columns; c++) {
+                if (this.tablero[r][c] === 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    calculate_date_difference(){
+        const diff = (this.end_time - this.init_time) * 1000;
+        var horas, minutos, segundos = 0;
+        horas = Math.trunc(diff/(60*60));
+        minutos = Math.trunc((diff/60) - horas*60);
+        segundos = diff%60;
+
+        return horas + ":" + minutos + ":" + segundos;
     }
     
 }
