60e8ef392f68b32a83419b4c57ec63033e44f883
UO291047 <uo291047@uniovi.es>
2023-12-17 12:21:46 +0100

    Noticias funcionando

--- js/noticias.js
+++ js/noticias.js
@@ -11,11 +11,21 @@ class Noticias{
         var tipoTexto = /text.*/;
         if (archivo.type.match(tipoTexto)) 
         {
-            var result;
             var lector = new FileReader();
 
-            lector.onload = function (evento) {
-                this.parseNoticias(lector.result);
+            lector.onload = function(event){
+                const noticias = lector.result.split('\n');
+                const mainElement = document.querySelector("main");
+                for(var n of noticias){
+                    var titulo = n.split('_')[0];
+                    var entradilla = n.split('_')[1];
+                    var texto = n.split('_')[2];
+                    var autor = n.split('_')[3];
+
+                    var noticia = document.createElement("p");
+                    noticia.innerHTML = " <h4>"+titulo+"</h4> " + " <p><strong>"+entradilla+"</strong></p> " + " <p>"+texto+"</p> " + " <p>"+autor+"</p> "
+                    mainElement.appendChild(noticia);
+                }
             }
 
             lector.readAsText(archivo);
@@ -26,8 +36,16 @@ class Noticias{
         }
     }
 
-    parseNoticias(result){
-        
+    crearNoticia(){
+        const mainElement = document.querySelector("main");
+        var titulo = document.getElementsByName("titulo")[0].value;
+        var entradilla = document.getElementsByName("entradilla")[0].value;
+        var texto = document.getElementsByName("contenido")[0].value;
+        var autor = document.getElementsByName("autor")[0].value;
+
+        var noticia = document.createElement("p");
+        noticia.innerHTML = " <h4>"+titulo+"</h4> " + " <p><strong>"+entradilla+"</strong></p> " + " <p>"+texto+"</p> " + " <p>"+autor+"</p> "
+        mainElement.appendChild(noticia);
     }
 
 }
