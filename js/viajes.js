be6d85267fb0949490fe54e07b2bc10826b2c518
UO291047 <uo291047@uniovi.es>
2023-12-18 16:46:09 +0100

    Dibujo de multiples rutas en un mapa dinamico hecho

--- js/viajes.js
+++ js/viajes.js
@@ -151,5 +151,85 @@ class Viajes{
         return htmlOutput;
       }
       
+      cargarArchivosKML(files) {
+        const mainElement = document.querySelector("main");
+
+        // Limpiamos el main por si ya tenía algo
+        while (mainElement.firstChild) {
+            mainElement.removeChild(mainElement.firstChild);
+        }
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoidW8yOTEwNDciLCJhIjoiY2xxOW1uOHd4MWcyNjJpcDdqcDFrZmRoNSJ9.wRJntK8K2RqcAqRrw0XE2g';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [this.longitud, this.latitud],
+            zoom: 14
+        });
+
+        var i = 0;
+        for (const file of files) {
+            const lector = new FileReader();
+
+            lector.onload = (event) => {
+                const resultXML = event.target.result;
+                this.procesarKML(resultXML, map, i);
+                i++;
+            };
+
+            lector.readAsText(file);
+        }
+    }
+
+    procesarKML(xmlContent, map, nRuta) {
+        const xmlDoc = $.parseXML(xmlContent);
+        const $xml = $(xmlDoc);
+    
+        const puntos = [];
+    
+        $xml.find('Placemark').each(function () {
+            const coordenadas = $(this).find('coordinates').text().split(' ');
+    
+            coordenadas.forEach(coord => {
+                const [longitud, latitud, altitud] = coord.split(',').map(parseFloat);
+                puntos.push([longitud, latitud, altitud]);
+            });
+        });
+    
+        const primerPunto = puntos[0];
+    
+        // Añadir la ruta al mapa centrando en el primer punto
+        map.on('load', function () {
+            map.addLayer({
+                'id': 'ruta' + nRuta,
+                'type': 'line',
+                'source': {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'Feature',
+                        'properties': {},
+                        'geometry': {
+                            'type': 'LineString',
+                            'coordinates': puntos
+                        }
+                    }
+                },
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                'paint': {
+                    'line-color': '#' + (Math.random() * 0xFFFFFF << 0).toString(16),
+                    'line-width': 8
+                }
+            });
+    
+            // Centrar el mapa en el primer punto
+            map.flyTo({
+                center: primerPunto,
+                zoom: 10
+            });
+        });
+    }
 
 }
