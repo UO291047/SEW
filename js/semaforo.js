db71e9b1429aa3e9eb399a8879b55c55debc9b0a
UO291047 <uo291047@uniovi.es>
2024-10-29 11:54:32 +0100

    Semaforo terminado

--- js/semaforo.js
+++ js/semaforo.js
@@ -8,10 +8,69 @@ class Semaforo{
 
     constructor(){
         this.difficulty = this.getRandomLevel();
+        this.createStructure();
     }
 
     getRandomLevel(){
         return this.levels[Math.floor(Math.random() * (3 - 0) + 0)]
+    }
+
+    createStructure(){
+        const container = document.querySelector("body main");
+        const title = document.createElement("h2");
+        title.textContent = "Juego Semáforo";
+        container.appendChild(title);
+
+        var div;
+        for(var i=0; i<this.lights; i++){
+            div = document.createElement("div");
+            container.appendChild(div)
+        }
+
+        const btStart = document.createElement("button");
+        btStart.textContent = "Arranque";
+        btStart.addEventListener("click", () => this.initSecuence());
+        const btStop = document.createElement("button");
+        btStop.textContent = "Reacción";
+        btStop.disabled = true;
+        btStop.addEventListener("click", () => this.stopReaction());
+        container.appendChild(btStart);
+        container.appendChild(btStop);
+
+        const p = document.createElement("p");
+        container.appendChild(p);
+    }
+
+    initSecuence(){
+        this.difficulty = this.getRandomLevel();
+        const main = document.querySelector("body main");
+        main.classList.add("load");
+        main.querySelector("button").disabled = true;
+        setTimeout(() => {
+            this.unload_moment = new Date().getTime();
+            this.endSecuence();
+        }, 1500 + this.difficulty * 1000);
+    }
+
+    endSecuence(){
+        const main = document.querySelector("body main");
+        main.classList.remove("load");
+        main.classList.add("unload");
+        main.querySelectorAll("button")[1].disabled = false;
+    }
+
+    stopReaction(){
+        this.clic_moment = new Date().getTime();
+        const diff = this.clic_moment - this.unload_moment;
+        var reactionTime = "Reacción: " + (diff * 0.001).toFixed(3) + " s";
+        var p = document.querySelector("body main p");
+        p.textContent = reactionTime;
+        const main = document.querySelector("body main");
+        main.appendChild(p);
+        main.classList.remove("load");
+        main.classList.remove("unload");
+        main.querySelector("button").disabled = false;
+        main.querySelectorAll("button")[1].disabled = true;
     }
 
 }
